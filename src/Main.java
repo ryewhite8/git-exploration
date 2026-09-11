e00ffc430a5a124ada8f77a5733e1a9acda31ec6
Au-Sulfer-Tellurium-Iodine-Nitrogen-fan <128Mleotta@frhsd.com>
2026-09-11 09:37:58 -0400

    Second reply

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("RB");
         System.out.println("hi rb, i'm lz");
+        System.out.println("Hi RB, Hi lz, I am MML");
     }
 }
